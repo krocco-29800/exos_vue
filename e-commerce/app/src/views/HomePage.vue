eae3f537238d532a80f189283ff1d8033e1b3490
69VFOMT104-14 <marcls29800@gmail.com>
2024-02-15 14:46:15 +0100

    UPD

---
+++ e-commerce/app/src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<script>
+import MyTeleportComponent from '../components/Content/MyTeleportComponent.vue';
+export default { 
+    name: "HomePage",
+    components: {
+        MyTeleportComponent
+    }
+}
+</script>
+
+<template>
+    <section>
+        <h1>I'm Home</h1>
+        <my-teleport-component/>
+    </section>
+</template>
